8638a05d6b06a363629f16902cfd6ba1919cb9ae
BicEv <bickbovev@gmail.com>
2026-06-09 20:03:21 +0300

    Added validation and ActiveRecord methods

--- src/main/java/ru/bicev/entity/MonitoredService.java
+++ src/main/java/ru/bicev/entity/MonitoredService.java
@@ -1,16 +1,50 @@
 package ru.bicev.entity;
 
+import java.util.List;
+
+import org.hibernate.validator.constraints.URL;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "monitored_services")
 public class MonitoredService extends PanacheEntity {
+
+    @NotBlank(message = "Name can not be empty")
     public String name;
+
+    @NotBlank(message = "Url can not be empty")
+    @URL(message = "Ivalid URL format")
     public String url;
-    public Integer checkIntervalSeconds;
-    public Integer expectedStatusCode;
-    public boolean active;
+
+    @Min(value = 1, message = "Interval must be greater or equal to 1 second")
+    public Integer checkIntervalSeconds = 60;
+
+    @Min(100)
+    @Max(599)
+    public Integer expectedStatusCode = 200;
+
+    public boolean active = true;
+
+    public static List<MonitoredService> findActive() {
+        return list("active", true);
+    }
+
+    public static List<MonitoredService> findInactive() {
+        return list("active", false);
+    }
+
+    public static List<MonitoredService> findNameLike(String name) {
+        return list("name ilike ?1", "%" + name + "%");
+    }
+
+    public static List<MonitoredService> findUrlLike(String url) {
+        return list("url ilike ?1", "%" + url + "%");
+    }
 
 }
