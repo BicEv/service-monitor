670f1438641674cea2b5477c3c33004cdc3130d8
BicEv <bickbovev@gmail.com>
2026-07-01 15:57:45 +0300

    Switched to returning dtos

--- src/main/java/ru/bicev/controller/HealthCheckLogResource.java
+++ src/main/java/ru/bicev/controller/HealthCheckLogResource.java
@@ -10,7 +10,9 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import ru.bicev.dto.HealthCheckLogDto;
 import ru.bicev.entity.HealthCheckLog;
+import ru.bicev.util.Mapper;
 
 @Path("/api/v1/logs")
 @Produces(MediaType.APPLICATION_JSON)
@@ -19,45 +21,45 @@ public class HealthCheckLogResource {
 
     @GET
     @Path("/service/{serviceId}")
-    public List<HealthCheckLog> getLogsByService(
+    public List<HealthCheckLogDto> getLogsByService(
             @PathParam("serviceId") Long serviceId,
             @QueryParam("page") @DefaultValue("0") int pageNum,
             @QueryParam("size") @DefaultValue("20") int size) {
-        return HealthCheckLog.findByServiceId(serviceId, pageNum, size);
+        return Mapper.toHealthCheckLogDtoList(HealthCheckLog.findByServiceId(serviceId, pageNum, size));
     }
 
     @GET
     @Path("/failures")
-    public List<HealthCheckLog> getAllFailures(
+    public List<HealthCheckLogDto> getAllFailures(
             @QueryParam("page") @DefaultValue("0") int pageNum,
             @QueryParam("size") @DefaultValue("20") int size) {
-        return HealthCheckLog.findAllFailures(pageNum, size);
+        return Mapper.toHealthCheckLogDtoList(HealthCheckLog.findAllFailures(pageNum, size));
     }
 
     @GET
     @Path("/service/{serviceId}/failures")
-    public List<HealthCheckLog> getFailuresByService(
+    public List<HealthCheckLogDto> getFailuresByService(
             @PathParam("serviceId") Long serviceId,
             @QueryParam("page") @DefaultValue("0") int pageNum,
             @QueryParam("size") @DefaultValue("20") int size) {
-        return HealthCheckLog.findFailuresByService(serviceId, pageNum, size);
+        return Mapper.toHealthCheckLogDtoList(HealthCheckLog.findFailuresByService(serviceId, pageNum, size));
     }
 
     @GET
     @Path("/latest")
-    public List<HealthCheckLog> getLatestLogs(
+    public List<HealthCheckLogDto> getLatestLogs(
             @QueryParam("limit") @DefaultValue("20") int limit,
             @QueryParam("page") @DefaultValue("0") int pageNum,
             @QueryParam("size") @DefaultValue("20") int size) {
-        return HealthCheckLog.findLastLogs(limit, pageNum, size);
+        return Mapper.toHealthCheckLogDtoList(HealthCheckLog.findLastLogs(limit, pageNum, size));
     }
 
     @GET
     @Path("/status")
-    public List<HealthCheckLog> getLogsByStatusCode(
+    public List<HealthCheckLogDto> getLogsByStatusCode(
             @QueryParam("statusCode") @DefaultValue("500") int statusCode,
             @QueryParam("page") @DefaultValue("0") int pageNum,
             @QueryParam("size") @DefaultValue("20") int size) {
-        return HealthCheckLog.findByStatusCode(statusCode, pageNum, size);
+        return Mapper.toHealthCheckLogDtoList(HealthCheckLog.findByStatusCode(statusCode, pageNum, size));
     }
 }
